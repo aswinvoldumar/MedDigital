6c59494f54ca349d8167a5c59efdd33078fb43cc
aswinvoldumar <aswin.voldumar@gmail.com>
2026-09-26 10:29:09 +0530

    removed A

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,13 +25,6 @@ export default function Navbar({ onOpenSearch }: NavbarProps) {
         >
           <Search size={16} strokeWidth={1.5} />
         </button>
-        <button
-          type="button"
-          aria-label="Accessibility"
-          className="grid h-9 w-9 place-items-center rounded-full border border-black/10 text-[11px] font-medium transition duration-300 ease-out hover:border-[#0D9488] hover:text-[#0D9488]"
-        >
-          <span aria-hidden>Aa</span>
-        </button>
       </div>
     </header>
   )
